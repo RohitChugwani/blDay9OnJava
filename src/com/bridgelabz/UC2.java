afe8a78b1b1667ff74944f8320a9a752bcd795a2
RohitChugwani <rchugwani3@gmail.com>
2022-06-09 11:07:58 +0530

    Created function to calculate daily wage of employee

---
+++ src/com/bridgelabz/UC2.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class UC2 extends UC1{
+    public static void  EmployeeWagePerDay()
+    {
+        int WAGE_PER_HOUR = 20;
+        int WORKING_HOURS = 8;
+        System.out.println("Wage of employee per day is:"+ WAGE_PER_HOUR * WORKING_HOURS );
+    }
+}
