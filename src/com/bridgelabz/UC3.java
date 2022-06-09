651033227d0cb4121c1c21107a185f7f48dbf1e3
RohitChugwani <rchugwani3@gmail.com>
2022-06-09 11:28:20 +0530

    created function to determine part time wage of an employee

---
+++ src/com/bridgelabz/UC3.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class UC3 {
+    public static void PartTimeWage()
+    {
+        int WAGE_PER_HOUR = 20;
+        int WORKING_HOURS = 4;
+        System.out.println("Wage of employee per day is:"+ WAGE_PER_HOUR * WORKING_HOURS );
+    }
+}
